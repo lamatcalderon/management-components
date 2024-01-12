fa796a94431a014bfbe7bbadd1c224237f50219c
lamat <lamat@debian-BULLSEYE-live-builder-AMD64>
2024-01-12 10:46:05 -0500

    fix

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,7 @@
-import VButton from "./components/base/v-button/v-button";
+import VButton from "./components/base/v-button/v-button.vue";
 import VGrid from "./components/base/v-grid/v-grid.vue";
 import VCard from "./components/base/v-card/v-card.vue";
 import VInput from "./components/base/v-input/v-input.vue";
-import VModal from "./components/common/v-modal/v-modal";
+import VModal from "./components/common/v-modal/v-modal.vue";
 import VOverlay from "./components/common/v-overlay/v-overlay.vue";
 export { VButton, VCard, VGrid, VInput, VModal, VOverlay };
--- dist/index.js
+++ dist/index.js
@@ -4,9 +4,18 @@ Object.defineProperty(exports, '__esModule', { value: true });
 
 var vue = require('vue');
 
-var vButton = vue.defineComponent({});
+var script$5 = vue.defineComponent({});
 
-var script$3 = vue.defineComponent({
+function render$5(_ctx, _cache, $props, $setup, $data, $options) {
+  return (vue.openBlock(), vue.createElementBlock("button", vue.normalizeProps(vue.guardReactiveProps(_ctx.$attrs)), [
+    vue.renderSlot(_ctx.$slots, "default")
+  ], 16 /* FULL_PROPS */))
+}
+
+script$5.render = render$5;
+script$5.__file = "src/components/base/v-button/v-button.vue";
+
+var script$4 = vue.defineComponent({
     setup() {
         const vgrid = vue.ref();
         vue.onMounted(() => {
@@ -24,12 +33,12 @@ var script$3 = vue.defineComponent({
     }
 });
 
-const _hoisted_1$2 = { class: "v-grid" };
-const _hoisted_2$1 = { ref: "vgrid" };
+const _hoisted_1$3 = { class: "v-grid" };
+const _hoisted_2$2 = { ref: "vgrid" };
 
-function render$3(_ctx, _cache, $props, $setup, $data, $options) {
-  return (vue.openBlock(), vue.createElementBlock("div", _hoisted_1$2, [
-    vue.createElementVNode("table", _hoisted_2$1, [
+function render$4(_ctx, _cache, $props, $setup, $data, $options) {
+  return (vue.openBlock(), vue.createElementBlock("div", _hoisted_1$3, [
+    vue.createElementVNode("table", _hoisted_2$2, [
       vue.createElementVNode("thead", null, [
         vue.renderSlot(_ctx.$slots, "thead")
       ]),
@@ -40,10 +49,10 @@ function render$3(_ctx, _cache, $props, $setup, $data, $options) {
   ]))
 }
 
-script$3.render = render$3;
-script$3.__file = "src/components/base/v-grid/v-grid.vue";
+script$4.render = render$4;
+script$4.__file = "src/components/base/v-grid/v-grid.vue";
 
-var script$2 = vue.defineComponent({
+var script$3 = vue.defineComponent({
     props: {
         title: {
             type: String,
@@ -52,26 +61,26 @@ var script$2 = vue.defineComponent({
     }
 });
 
-const _hoisted_1$1 = { class: "v-card" };
-const _hoisted_2 = { class: "card-title" };
-const _hoisted_3 = /*#__PURE__*/vue.createElementVNode("span", null, null, -1 /* HOISTED */);
+const _hoisted_1$2 = { class: "v-card" };
+const _hoisted_2$1 = { class: "card-title" };
+const _hoisted_3$1 = /*#__PURE__*/vue.createElementVNode("span", null, null, -1 /* HOISTED */);
 
-function render$2(_ctx, _cache, $props, $setup, $data, $options) {
-  return (vue.openBlock(), vue.createElementBlock("div", _hoisted_1$1, [
-    vue.createElementVNode("div", _hoisted_2, [
+function render$3(_ctx, _cache, $props, $setup, $data, $options) {
+  return (vue.openBlock(), vue.createElementBlock("div", _hoisted_1$2, [
+    vue.createElementVNode("div", _hoisted_2$1, [
       vue.createElementVNode("span", null, vue.toDisplayString(_ctx.title), 1 /* TEXT */),
-      _hoisted_3
+      _hoisted_3$1
     ]),
     vue.renderSlot(_ctx.$slots, "content")
   ]))
 }
 
-script$2.render = render$2;
-script$2.__file = "src/components/base/v-card/v-card.vue";
+script$3.render = render$3;
+script$3.__file = "src/components/base/v-card/v-card.vue";
 
 const APP_PREFIX = "mc";
 
-var script$1 = vue.defineComponent({
+var script$2 = vue.defineComponent({
     props: {
         label: {
             type: String,
@@ -97,9 +106,9 @@ var script$1 = vue.defineComponent({
     }
 });
 
-const _hoisted_1 = ["placeholder"];
+const _hoisted_1$1 = ["placeholder"];
 
-function render$1(_ctx, _cache, $props, $setup, $data, $options) {
+function render$2(_ctx, _cache, $props, $setup, $data, $options) {
   return (vue.openBlock(), vue.createElementBlock("div", {
     class: vue.normalizeClass(`${_ctx.APP_PREFIX}-v-input`)
   }, [
@@ -124,16 +133,16 @@ function render$1(_ctx, _cache, $props, $setup, $data, $options) {
           disabled: _ctx.disabled,
         },
         placeholder: _ctx.placeholder
-      }), null, 16 /* FULL_PROPS */, _hoisted_1)
+      }), null, 16 /* FULL_PROPS */, _hoisted_1$1)
     ], 2 /* CLASS */)
   ], 2 /* CLASS */))
 }
 
-script$1.render = render$1;
-script$1.__scopeId = "data-v-2488f5a9";
-script$1.__file = "src/components/base/v-input/v-input.vue";
+script$2.render = render$2;
+script$2.__scopeId = "data-v-2488f5a9";
+script$2.__file = "src/components/base/v-input/v-input.vue";
 
-var vModal = vue.defineComponent({
+var script$1 = vue.defineComponent({
     props: {
         isOpen: {
             type: Boolean,
@@ -145,6 +154,38 @@ var vModal = vue.defineComponent({
         }
     }
 });
+
+const _hoisted_1 = { class: "header" };
+const _hoisted_2 = { class: "title" };
+const _hoisted_3 = /*#__PURE__*/vue.createElementVNode("div", { class: "options" }, [
+  /*#__PURE__*/vue.createElementVNode("a", { href: "javascript:void(0)" }, [
+    /*#__PURE__*/vue.createElementVNode("em", { class: "fas fa-plus" })
+  ])
+], -1 /* HOISTED */);
+const _hoisted_4 = { class: "content" };
+const _hoisted_5 = { class: "footer" };
+
+function render$1(_ctx, _cache, $props, $setup, $data, $options) {
+  return (vue.openBlock(), vue.createElementBlock("div", {
+    class: vue.normalizeClass(["modal", { 'is-open': _ctx.isOpen, 'is-close': !_ctx.isOpen }])
+  }, [
+    vue.createElementVNode("div", _hoisted_1, [
+      vue.createElementVNode("div", null, [
+        vue.createElementVNode("span", _hoisted_2, vue.toDisplayString(_ctx.title), 1 /* TEXT */)
+      ]),
+      _hoisted_3
+    ]),
+    vue.createElementVNode("div", _hoisted_4, [
+      vue.renderSlot(_ctx.$slots, "content")
+    ]),
+    vue.createElementVNode("div", _hoisted_5, [
+      vue.renderSlot(_ctx.$slots, "footer")
+    ])
+  ], 2 /* CLASS */))
+}
+
+script$1.render = render$1;
+script$1.__file = "src/components/common/v-modal/v-modal.vue";
 
 var script = vue.defineComponent({
     props: {
@@ -166,10 +207,10 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
 script.render = render;
 script.__file = "src/components/common/v-overlay/v-overlay.vue";
 
-exports.VButton = vButton;
-exports.VCard = script$2;
-exports.VGrid = script$3;
-exports.VInput = script$1;
-exports.VModal = vModal;
+exports.VButton = script$5;
+exports.VCard = script$3;
+exports.VGrid = script$4;
+exports.VInput = script$2;
+exports.VModal = script$1;
 exports.VOverlay = script;
 //# sourceMappingURL=index.js.map
